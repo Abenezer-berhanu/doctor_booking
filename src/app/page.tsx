01d19ca34a2a26a7a3c37cf7f71c997daeff06af
Abenezer <abenezerberhanu40@gmail.com>
2024-01-02 01:48:52 +0300

    add grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { HiMiniLightBulb } from "react-icons/hi2";
 import { IoStar } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -115,14 +116,14 @@ export default function Home() {
             </span>
           </CardContent>
         </Card>
-        <div className="flex max-w-[1300px] mx-auto justify-center max-sm:flex-col sml:justify-evenly my-10 p-2 items-center">
-          <div className="w-1/2 max-sml:w-full rounded-md">
+        <div className="flex max-w-[1300px] mx-auto justify-center max-sm:flex-col sml:justify-evenly my-5 p-2 items-center bg-bg_gray">
+          <div className="w-1/2 max-sml:w-full rounded-lg">
             <Image
               src={"/terapist.png"}
               alt="therapist"
               width={300}
               height={300}
-              className="w-full h-full rounded-md sml:scale-75"
+              className="w-full h-full rounded-lg sml:scale-75"
             />
           </div>
           <div className="w-1/2 max-sml:w-full flex flex-col gap-4">
@@ -149,7 +150,86 @@ export default function Home() {
           </div>
         </div>
         <Separator />
-        
+        <div className="text-center my-16">
+          <h1 className="font-bold text-3xl mb-4">
+            Merdo <span className="bg-primary text-white">Immediate care</span>{" "}
+            Facilitates <br /> Provide Quick Care and Relief
+          </h1>
+          <p className="text-text_gray max-w-[700px] text-center mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            saepe vitae vero corrupti omnis vel voluptatem itaque rerum fuga
+            dolorem, natus quas sapiente, quos quod possimus minus libero totam
+            quam.
+          </p>
+          <div className="grid grid-cols-3 grid-rows-8 my-8 max-w-[800px] max-h-[800px] mx-auto gap-5">
+            <div className="w-full h-[200px] bg-cover bg-[url('/grid1.jpg')] rounded-lg row-start-2 row-span-2 shadow-md border border-slate-300"></div>
+            <div className="w-full p-2 row-span-4 shadow-md border border-slate-300">
+              <Image
+                src={"/drug.png"}
+                width={40}
+                height={40}
+                alt="lung"
+                className="m-4"
+              />
+              <strong>Wound healing genes</strong>
+              <p className="text-text_gray text-sm text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus neque nemo repudiandae atque porro exercitationem
+                consequuntur eum dolores quae, voluptates molestias est, aperiam
+                minima beatae consectetur totam quas. Architecto, maiores?
+              </p>
+              <Button variant={"secondary"} className="rounded-full text-start">
+                <FaPlus />
+              </Button>
+            </div>
+            <div className="w-full bg-white p-2 row-start-2 row-span-4 shadow-md border border-slate-300">
+              <Image
+                src={"/heart.png"}
+                width={40}
+                height={40}
+                alt="lung"
+                className="m-4"
+              />
+              <strong>Generic Strategy</strong>
+              <p className="text-text_gray text-sm text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus neque nemo repudiandae atque porro exercitationem
+                consequuntur eum dolores quae, voluptates molestias est, aperiam
+                minima beatae consectetur totam quas. Architecto, maiores?
+              </p>
+              <Button variant={"secondary"} className="rounded-full text-start">
+                <FaPlus />
+              </Button>
+            </div>
+            <div className="w-full row-start-4 row-span-4 shadow-md border border-slate-300 p-2">
+              <Image
+                src={"/lung.png"}
+                width={40}
+                height={40}
+                alt="lung"
+                className="m-4"
+              />
+              <strong>Blood Clothing Disorder</strong>
+              <p className="text-text_gray text-sm text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus neque nemo repudiandae atque porro exercitationem
+                consequuntur eum dolores quae, voluptates molestias est, aperiam
+                minima beatae consectetur totam quas. Architecto, maiores?
+              </p>
+              <Button variant={"secondary"} className="rounded-full text-start">
+                <FaPlus />
+              </Button>
+            </div>
+            <div className="w-full bg-indigo-900 text-white rounded-md row-start-5 row-span-3 shadow-md border border-slate-300 p-5 text-start font-bold text-3xl">
+              Merdio Care <br />
+              Facilitates Provide Quick
+              <Button className="text-xs rounded-full">
+                view all services
+              </Button>
+            </div>
+            <div className="w-full bg-black row-start-6 row-span-2 shadow-md border border-slate-300 bg-[url('/allergy.jpg')] bg-cover"></div>
+          </div>
+        </div>
       </div>
     </>
   );
