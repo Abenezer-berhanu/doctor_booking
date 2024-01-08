af5855d12aee7d62d9143646682a00d216924cdc
Abenezer <abenezerberhanu40@gmail.com>
2024-01-08 22:18:12 +0300

    finish about

--- src/components/AboutLanding/AboutFooter.tsx
+++ src/components/AboutLanding/AboutFooter.tsx
@@ -5,9 +5,9 @@ import { FaUserDoctor } from "react-icons/fa6";
 function AboutFooter() {
   return (
     <div className="my-5 w-full relative mx-auto min-h-[400px] flex items-center">
-      <div className="w-1/2 bg-primary h-full right-0 absolute -z-10"></div>
+      <div className="w-1/2 sml:bg-primary h-full right-0 absolute -z-10"></div>
       <div className="w-full h-full p-5 grid gap-2 sml:grid-cols-5">
-        <div className="sml:col-span-2 flex flex-col gap-3 text-start">
+        <div className="sml:col-span-2 flex flex-col gap-3 text-start max-sml:items-center">
           <p className="text-green_color text-sm font-semibold">| Services</p>
           <h1 className="text-2xl sml:text-4xl text-primary tracking-wider font-bold">
             We Provides Best <br /> Services in Hospital
@@ -19,7 +19,7 @@ function AboutFooter() {
             beatae.
           </p>
         </div>
-        <div className="col-span-1 bg-bg_white_gray flex flex-col gap-3 items-center p-2">
+        <div className="col-span-1 bg-bg_white_gray dark:bg-bg_black flex flex-col gap-3 items-center p-2">
           <FaDiagnoses className="text-4xl text-primary" />
           <p className="font-bold text-center text-lg sml:text-xl text-primary">
             Diagnosis
@@ -29,7 +29,7 @@ function AboutFooter() {
             sit dolores repellat dignissimos dolorum, architecto ad esse
           </p>
         </div>
-        <div className="col-span-1 bg-bg_white_gray flex flex-col gap-3 items-center p-2">
+        <div className="col-span-1 bg-bg_white_gray dark:bg-bg_black flex flex-col gap-3 items-center p-2">
           <GiMedicines className="text-4xl text-primary" />
           <p className="font-bold text-center text-lg sml:text-xl text-primary">
             Medicine
@@ -39,7 +39,7 @@ function AboutFooter() {
             sit dolores repellat dignissimos dolorum, architecto ad esse
           </p>
         </div>
-        <div className="col-span-1 bg-bg_white_gray flex flex-col gap-3 items-center p-2">
+        <div className="col-span-1 bg-bg_white_gray dark:bg-bg_black flex flex-col gap-3 items-center p-2">
           <FaUserDoctor className="text-4xl text-primary" />
           <p className="font-bold text-center text-lg sml:text-xl text-primary">
             Expert Doctor
