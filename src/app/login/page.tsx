813310af300f7c0e3bf00aff0d0c71abb8c8b5c7
Abenezer <abenezerberhanu40@gmail.com>
2024-01-13 00:47:48 +0300

    add login up page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { signIn, signOut } from "@/lib/auth";
+import Link from "next/link";
+import { IoIosLock } from "react-icons/io";
 
 function page() {
   const handleSubmit = async () => {
@@ -16,7 +18,7 @@ function page() {
   };
   return (
     <>
-      <form action={handleSubmit}>
+      {/* <form action={handleSubmit}>
         <Button>login</Button>
       </form>
       <form action={handleGoogleLogin}>
@@ -24,7 +26,53 @@ function page() {
       </form>
       <form action={handleLogout}>
         <Button>logout</Button>
-      </form>
+      </form> */}
+
+      <div className="m-4 max-w-[400px] mx-auto p-4 rounded-md flex flex-col items-center justify-evenly shadow-md shadow-primary min-h-[500px]">
+        <div className="flex flex-col gap-3">
+          <span className="text-2xl font-bold flex items-center justify-center tracking-wider">
+            <IoIosLock className="text-yellow-500" /> Auth
+          </span>
+          <p className="text-sm font-semibold">Welcome back!</p>
+        </div>
+        <form action="" className="flex flex-col gap-4 w-full justify-start">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="font-semibold flex gap-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="py-2 indent-3 text-sm rounded-md bg-gray-50 w-full outline-slate-100"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password" className="font-semibold flex gap-2">
+              Password
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="py-2 indent-3 text-sm rounded-md bg-gray-50 w-full outline-slate-100"
+            />
+          </div>
+          <p>Forgot Password?</p>
+          <Button className="">Login</Button>
+        </form>
+        <div className="grid grid-cols-2 gap-2">
+          <form
+            action=""
+            className="w-full border-2 border-slate-50 flex items-center justify-center"
+          ></form>
+          <form
+            action=""
+            className="w-full border-2 border-slate-50 flex items-center justify-center"
+          ></form>
+        </div>
+        <small>
+          Don{"'"}t have an Account?<Link href={"/signup"}>Sign up</Link>
+        </small>
+      </div>
     </>
   );
 }
